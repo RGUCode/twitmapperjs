350190c33dd9402c9fff6492ca62d7dbbfde1b81
John <wildfireone@gmail.com>
2016-03-12 21:59:59 +0000

    added env variables

--- twitscraper.js
+++ twitscraper.js
@@ -2,12 +2,12 @@ var Twitter = require('twitter');
 console.log("created twitter");
 
 var client = new Twitter({
-  console.log("created twitter auth)");
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
+console.log("created twitter auth)");
 
 client.stream('statuses/filter', {track: 'javascript'}, function(stream) {
   stream.on('data', function(tweet) {
