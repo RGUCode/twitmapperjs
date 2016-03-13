bc42a390e1fac37209e36343c2989885f0e5d14a
John <wildfireone@gmail.com>
2016-03-13 21:22:35 +0000

    added servertweet db

--- twitscraper.js
+++ twitscraper.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://localhost:27017/test';
+var mongoURL = 'mongodb://localhost:27017/test';
 var Twitter = require('twitter');
 var counter = 0;
 
