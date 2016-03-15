95dc7f0030a328aa39d6f8c7f56d11527f63772b
John <wildfireone@gmail.com>
2016-03-15 21:27:47 +0000

    added showdata

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ var showStats = function(db, callback,res) {
   var html = '<h2> Stats </h2>';
   db.collection('tweets').count(function(err, count){
     html += '<h2> Document Count </h2>';
-    html += '<p>'+JSON.stringify(count))+'</p>';
+    html += '<p>'+JSON.stringify(count)+'</p>';
     db.stats(function(err, stats){
       html += '<p>'+JSON.stringify(stats)+'</p>';
       callback(html,res,db);
