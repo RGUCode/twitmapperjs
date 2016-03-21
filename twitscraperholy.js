d7195352e3a552403385db8e5f25515fe23f1700
John <wildfireone@gmail.com>
2016-03-21 21:30:21 +0000

    added tracker for holyrood

--- twitscraperholy.js
+++ twitscraperholy.js
@@ -6,8 +6,8 @@ var Twitter = require('twitter');
 var counter = 0;
 
 var client = new Twitter({
-  consumer_key: 'KXdzujUy8NHjC1h2obqNPtMVL',
-  consumer_secret: '7f24ZangnSfEx14iJ1MsghO5AuoHHZJHTLiazNiznthdTfuKJE',
+  consumer_key: 'zSN8z9oDC5xG7Ticl3pnPHtKi',
+  consumer_secret: 'Pg06j6wIiC3pdRhhbAUI3gOaDni3jXHMUMo79mF5IymZ2FKHh4',
   access_token_key: '14812487-06dGq8Lr1VkKZS21iuuZ0tr36tg5oi9yycWFcjpnV',
   access_token_secret: 'fFFOlemWQbnS7n56rtppDLR0TCy4zrrgmheL81abj6vA2',
 });
