aa80bd8f3a584206eacadae250f22bff614d31b5
John <wildfireone@gmail.com>
2016-03-21 21:50:15 +0000

    added apps and map for holyrood

--- twitscraperholy.js
+++ twitscraperholy.js
@@ -36,7 +36,7 @@ client.stream('statuses/filter', {track: 'holyrood16, #holyrood16, #Holyrood16'}
 var insertDocument = function(db, newtweet, callback) {
    db.collection('holyrood16').insertOne( newtweet, function(err, result) {
     assert.equal(err, null);
-    //console.log("Inserted a document into the tweets collection.");
+    console.log("Inserted a document into the tweets collection.");
     callback();
   });
 };
